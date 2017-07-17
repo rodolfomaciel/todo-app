737abf5e5bd3df716118b131f43eec2d3d798249
Rodolfo Maciel <rodolfomaciel@outlook.com>
2017-07-17 08:53:57 -0300

    todo-app

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import PageHeader from "../template/pageHeader"
+
+export default props => (
+	<div>
+           <PageHeader name="Sobre" small="A Empresa"></PageHeader>
+	   <h2>A empresa</h2>
+	   Lore1
+	</div>		
+)
